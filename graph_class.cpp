e2c3d5305cdae7ffa6be0f7aa767182f6ba382e0
A. Taylan Cemgil <taylan.cemgil@boun.edu.tr>
2017-03-27 21:31:51 +0300

    Add files via upload

---
+++ graph_class.cpp
@@ -0,0 +1,78 @@
+//============================================================================
+// Name        : graph_class.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>     // std::cout
+#include <fstream>      // std::ifstream
+#include <list>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void PrintList(vector< list<int> >& lst) {
+	  for (int i=0; i<lst.size(); i++) {
+		  cout << i << ": ";
+		  for (list<int>::iterator ip=lst[i].begin(); ip!=lst[i].end(); ip++) {
+			  cout << *ip << " ";
+		  }
+		  cout << endl;
+	  }
+}
+
+
+class Graph {
+	int numNodes;
+	vector< list<int> > outlist;
+	vector< list<int> >  inlist;
+public:
+	Graph(int N) {
+		numNodes = N;
+		outlist.resize(N);
+		inlist.resize(N);
+	}
+
+	Graph(string fn) {
+		ReadFromFile(fn);
+	}
+
+
+	void Print() {
+		cout << "Outlist:" << endl;
+		PrintList(outlist);
+		cout << "Inlist:" << endl;
+		PrintList(inlist);
+	}
+
+	int InDegree(int vertex) {
+
+	}
+
+	void ReadFromFile(string fn) {
+		ifstream ifs;
+		int s, t;
+
+		ifs.open (fn, std::ifstream::in);
+		ifs >> numNodes;
+		inlist.resize(numNodes);
+		outlist.resize(numNodes);
+
+		 ifs >> s >> t;
+		 while (ifs.good()) {
+			outlist[s].push_back(t);
+			inlist[t].push_back(s);
+			ifs >> s >> t;
+		 }
+		 ifs.close();
+	}
+};
+
+int main () {
+	Graph g("/Users/cemgil/Downloads/data.txt");
+	g.Print();
+	return 0;
+}
